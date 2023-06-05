56668377edb6675be90d7d2f665a4eaff970e32e
Reina Orikasa <tammichael76@gmail.com>
2023-06-05 12:30:10 +0900

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+import tailwind from "@astrojs/tailwind";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [storyblok({
+    accessToken: env.STORYBLOK_TOKEN,
+    components: {
+      // Add your components here
+      blogPost: 'storyblok/BlogPost',
+      blogPostList: 'storyblok/BlogPostList',
+      page: 'storyblok/Page'
+    },
+    apiOptions: {
+      // Choose your Storyblok space region
+      region: "eu" // optional,  or 'eu' (default)
+    }
+  }), tailwind()]
+});
